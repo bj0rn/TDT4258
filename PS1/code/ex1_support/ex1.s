2be5eca8efdaeccab8155e44e92f4a98c73e04a4
Bjørn <meister.bj0rn@gmail.com>
2014-01-25 18:48:33 +0100

    Fixes the old code

--- PS1/code/ex1_support/ex1.s
+++ PS1/code/ex1_support/ex1.s
@@ -86,14 +86,15 @@ _reset:
 	      BL setup_leds
 	      BL setup_buttons
 	      BL setup_interrupts
-		 ////// BL polling
-          
-		  B .
+		  BL setup_energy_mode
+		 // BL polling
+          WFI
+		  B . //just in case
           
 		.thumb_func
 setup_gpio_clk:
-		 //Load CPU base address
-		 ldr r1, cmu_base_addr
+		 //Load CMU base address
+		 ldr r1, =CMU_BASE
 
 		 //load current value of HFPERCLK ENBALE
 		 ldr r2, [r1, #CMU_HFPERCLKEN0]
@@ -105,11 +106,13 @@ setup_gpio_clk:
 
 		 //Store new value 
 		 str r2, [r1, #CMU_HFPERCLKEN0]
+		
+		 BX LR
 
 		.thumb_func
 setup_leds:
 		//load gpio pa base address
-		ldr r1, gpio_pa_base_addr
+		ldr r1, =GPIO_PA_BASE
 		
 		//set high drive strength
 		mov r2, #0x2
@@ -119,66 +122,77 @@ setup_leds:
 		ldr r2, gpio_output
 		str r2, [r1, #GPIO_MODEH]
 
-		//set pins high
+	   //set pins high
 	   // mov r2, #0xff
-		//lsl r2, r2, #8
-   		//str r2, [r1, #GPIO_DOUT]
+	   // lsl r2, r2, #8
+   	   // str r2, [r1, #GPIO_DOUT]
+
+		BX LR
 
 		.thumb_func
 setup_buttons:
-		ldr r1, gpio_pc_base_addr
+		ldr r1, =GPIO_PC_BASE 
  		ldr r2, gpio_input
 		str r2, [r1, #GPIO_MODEL]
 		mov r2, #0xff
 		str r2, [r1, #GPIO_DOUT]
 
+		BX LR
+
+	  .thumb_func
+setup_energy_mode: 
+		ldr r1, =SCR
+		mov r2, #6
+		str r2, [r1]
+       
+	    BX LR
 
 
 	  .thumb_func
 polling:
-		ldr r1, gpio_pc_base_addr
-		ldr r2, gpio_pa_base_addr
+		ldr r1, =GPIO_PC_BASE
+		ldr r2, =GPIO_PA_BASE
 while: 	
 		ldr r3, [r1, #GPIO_DIN]
-		//mov r3, 0xDF
 		lsl r3, r3, #8
 		str r3, [r2, #GPIO_DOUT]
 		b while
 
 	.thumb_func
 setup_interrupts:
-	  //LOAD base address for port C
-	  ldr r1, gpio_pc_base_addr
+	  //LOAD GPIO base
+	  ldr r1, =GPIO_BASE
 	 
 	 //Load interrupt enable value
 	  ldr r2, gpio_pc_interrupt
 	 
 	 //Store value
-	  ldr r2, [r1, #GPIO_EXTIPSELL]
+	  str r2, [r1, #GPIO_EXTIPSELL]
 	 
 	 //Setup interrupt falling edge
 	  mov r2, #0xff
-	  str r2, [r1, GPIO_EXTIFALL]
+	  str r2, [r1, #GPIO_EXTIFALL]
 	 
 	 //setup interrupt on rising edge
 	  mov r2, #0xff
-	  str r2, [r1, GPIO_EXTIRISE]
+	  str r2, [r1, #GPIO_EXTIRISE]
 	  
 	 //Enable interrupt generation
 	  mov r2, #0xff
-	  str r2, [r1, GPIO_IEN]
+	  str r2, [r1, #GPIO_IEN]
 
      //Enable interrupt handling
-     ldr r1, iser0
+     ldr r1, =ISER0
 	 ldr r2, enable_interrupt_handling
 	 str r2, [r1]
-	  
+	 
+	 BX LR
+
+
 
 enable_interrupt_handling:
 		.word 0x802
 
-iser0: 
-	   .word ISER0
 
 gpio_pc_interrupt:
 		.word 0x22222222
@@ -189,14 +203,6 @@ gpio_output:
 gpio_input: 
 			.word 0x33333333
 
-gpio_pa_base_addr:
-			.long GPIO_PA_BASE
-
-gpio_pc_base_addr:
-			.long GPIO_PC_BASE
-
-cmu_base_addr: 
-			.long CMU_BASE
 
 
 
@@ -214,21 +220,38 @@ cmu_base_addr:
 	
         .thumb_func
 gpio_handler:  
-		 ldr r1, gpio_pa_base_addr
-		 ldr r2, gpio_pc_base_addr
-	     
-		 ldr r3, [r2, #GPIO_DIN]
-		 lsl r3, r3, #8
-		 str r3, [r1, #GPIO_DOUT]
+		//Clear interrupt handler
+		ldr r1, =GPIO_BASE
+		mov r2, #0xff
+		str r2, [r1, #GPIO_IFC]
+		
+        //Turn on buttons
+		ldr r1, =GPIO_PC_BASE
+	    ldr r2, =GPIO_PA_BASE
 
-         mov r3, #0xff
-		 str r3, [r2, GPIO_IFC]
-
-	    //  b .  // do nothing
+	    ldr r3, [r1, #GPIO_DIN]
+	    lsl r3, r3, #8
+		str r3, [r2, #GPIO_DOUT]
+        
+		BX LR
+		NOP 
+	
 	
 	/////////////////////////////////////////////////////////////////////////////
-	
+
+	.thumb_func
+delay:
+	movw r3, #0xFFFF
+	movt r3, #0x0000
+delay_loop:
+	cbz r3, delay_exit
+	sub r3, r3, #1
+	B delay_loop
+delay_exit:
+	BX LR
+
+
         .thumb_func
 dummy_handler:  
-       // b .  // do nothing
+     b .  // do nothing
 
