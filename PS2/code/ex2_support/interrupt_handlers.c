cddb9a37ddc2aa16aabcb7a0ae71960c5971126f
Bjørn <meister.bj0rn@gmail.com>
2014-02-18 13:57:50 +0100

    Clear interrupts in interrupt handlers

--- PS2/code/ex2_support/interrupt_handlers.c
+++ PS2/code/ex2_support/interrupt_handlers.c
@@ -15,11 +15,22 @@ void __attribute__ ((interrupt)) TIMER1_IRQHandler()
 /* GPIO even pin interrupt handler */
 void __attribute__ ((interrupt)) GPIO_EVEN_IRQHandler() 
 {
+	/* Clear pending interrupts */
+	*GPIO_IFC = *GPIO_IF;
+			    
+
     /* TODO handle button pressed event, remember to clear pending interrupt */
 }
 
 /* GPIO odd pin interrupt handler */
 void __attribute__ ((interrupt)) GPIO_ODD_IRQHandler() 
 {
+	/*Clear pending interrupts*/
+	*GPIO_IFC = *GPIO_IF;
+			    
+			    
+
+
     /* TODO handle button pressed event, remember to clear pending interrupt */
+	/* TODO set input and output pins for the joystick */
 }
