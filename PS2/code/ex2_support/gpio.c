74635990d0e47a54318072e74e00c0ae382a21fc
Bjørn <meister.bj0rn@gmail.com>
2014-02-18 13:35:58 +0100

    Adds code for configuring GPIO

--- PS2/code/ex2_support/gpio.c
+++ PS2/code/ex2_support/gpio.c
@@ -6,15 +6,29 @@
 /* function to set up GPIO mode and interrupts*/
 void setupGPIO()
 {
-  /* TODO set input and output pins for the joystick */
+	*CMU_HFPERCLKEN0 |= (1 << 13); /* Enable GPIO clock*/
+  	
+	/*Configure gamepad LEDs*/
+	*GPIO_PA_CTRL = 2; /* Set high drive strength */
+	*GPIO_PA_MODEH = 0x55555555; /*Set pins A8-15 as output*/
+	
+	/*Configure gamepad buttons */
+	*GPIO_PC_MODEL = 0x33333333; /*Set pins C0-7 as input*/
+	*GPIO_PC_DOUT = 0xff; /*Enable internal pull-up*/
+
+	/* Configure interrupts */
+	*GPIO_EXTIPSELL = 0x22222222; /*Enable interrupts*/
+	*GPIO_EXTIFALL = 0xff; /*Sense falling edge */
+	*GPIO_EXTIRISE = 0xff; /*Sense rising edge */
+	*GPIO_IEN = 0xff; /*Enable interrupt generation*/
+	*ISER0 = 0x802; /*Enable interrupt handling*/
+
+
     
   /* Example of HW access from C code: turn on joystick LEDs D4-D8
      check efm32gg.h for other useful register definitions
   */
-  *CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_GPIO; /* enable GPIO clock*/
-  *GPIO_PA_CTRL = 2;  /* set high drive strength */
-  *GPIO_PA_MODEH = 0x55555555; /* set pins A8-15 as output */
-  *GPIO_PA_DOUT = 0x0700; /* turn on LEDs D4-D8 (LEDs are active low) */
+  //*GPIO_PA_DOUT = 0x0700; /* turn on LEDs D4-D8 (LEDs are active low) */
 }
 
 
