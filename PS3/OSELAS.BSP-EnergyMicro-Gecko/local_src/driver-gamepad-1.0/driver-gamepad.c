510c38b162c9b4e07587ddb3233b0c7b346e237f
Bjørn <meister.bj0rn@gmail.com>
2014-04-10 17:18:45 +0200

    Test

--- PS3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ PS3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -5,6 +5,45 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/module.h>
+#include <linux/cdev.h>
+
+
+
+/*Prototypes*/
+
+static int open_driver(struct inode *node, struct file *filp);
+static int release_driver(struct inode *inode, struct file *filp);
+static ssize_t read_driver(struct file *filp, char __user *buff, size_t count, loff_t *offp);
+static ssize_t write_driver(struct file *filp, const char __user *buf, size_t count, loff_t *offp);
+
+/*Function pointers*/
+int (*open_driver)(struct inode*, struct file *);
+int (*release_driver)(struct inode *, struct file*);
+ssize_t (*read_driver)(struct file*, char __user *, size_t, loff_t); 
+ssize_t (*write_driver)(struct file*, char __user*, size_t, loff_t);
+
+
+/*Structures*/
+struct cdev *driver_cdev;
+struct class *cl;
+dev_t devno;
+int tdt4258_major;
+int tdt4258_minor;
+
+
+
+
+static struct file_operations fops = {
+	.owner = THIS_MODULE, 
+	.read = read_driver,
+	.write = write_driver,
+	.open = open_driver,
+	.release = release_driver
+};
+
+
 
 /*
  * template_init - function to insert this module into kernel space
@@ -15,46 +54,39 @@
  * Returns 0 if successfull, otherwise -1
  */
 
-static int my_probe(struct platform_device *dev){
-	printk("Hello world, here is your new module speaking\n");
-	return 0;
-}
-
-
-static int my_remove(struct platform_device *dev){
-	printk("Leaving\n");
-	return 0;
-}
-
-
-static const struct of_device_id my_of_match[] = {
-	{.compitable = "tdt4258", }, 
-	{},
-};
-
-MODULE_DEVICE_TABLE(of, my_of_match);
-
-static struct platform_driver my_driver = {
-	.probe = my_probe,
-	.remove = my_remove, 
-	.driver = {
-		.name = "my",
-		.owner = THIS_MODULE,
-		.of_match_table = my_of_match,
-	},
-};
-
-platform_driver_register(&my_driver);
-
-
-
-/* 
-static int __init template_init(void)
+static int __init init_driver(void)
 {
+	
+	
+	int err, devno = MKDEV(tdt4258_major,tdt4258_minor);
+	err = register_chrdev_region(devno, 1, "tdt4258");
+	
+	cdev_init(&driver_cdev, &fops);  	    //Init the cdev
+	driver_cdev.owner = THIS_MODULE;
+	driver_cdev.ops = &fops;
+	err = cdev_add(&driver_cdev, devno, 1); //Register to kernel
+	
+
+	//Fail nicely
+	if(err){
+		printk(KERN_NOTICE"Error %d", err);
+	}else {
+		printk("Device added to the kernel sucessfully\n");
+	}
+
+	
+	cl = class_create(THIS_MODULE, "tdt4258");
+	device_create(cl, NULL, devno, NULL, "tdt4258");
+	
+
+
+	
+	
 	printk("Hello World, here is your module speaking or is it ?\n");
 	return 0;
 }
-*/
+
+
 /*
  * template_cleanup - function to cleanup this module from kernel space
  *
@@ -62,15 +94,37 @@ static int __init template_init(void)
  * code from a running kernel
  */
 
-/* 
-static void __exit template_cleanup(void)
+static void __exit cleanup_driverid)
 {
 	 printk("Short life for a small module...\n");
 }
-*/
-//module_init(template_init);
-//module_exit(template_cleanup);
 
-//MODULE_DESCRIPTION("Small module, demo only, not very useful.");
-//MODULE_LICENSE("GPL");
+/*User program opens the driver*/
+static int open_driver(struct inode *node, struct file *filp){
+
+}
+
+/*User program close the driver*/
+static int release_driver(struct inode *inode, struct file *filp){
+
+}
+
+
+/*User program reads from the driver */
+
+static ssize_t read_driver(struct file *filp, char __user *buff, size_t count, loff_t *offp){
+
+}
+
+
+static ssize_t write_driver(struct file *filp, const char __user *buf, size_t count, loff_t *offp){
+
+}
+
+
+module_init(init_driver);
+module_exit(cleanup_driver);
+
+MODULE_DESCRIPTION("Small module, demo only, not very useful.");
+MODULE_LICENSE("GPL");
 
