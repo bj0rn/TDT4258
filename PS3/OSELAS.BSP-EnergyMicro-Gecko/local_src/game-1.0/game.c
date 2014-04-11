7ddd93b6665868a4499b49d8426ed863e3e8b3f1
Hans Melby <hanshell@stud.ntnu.no>
2014-04-11 14:10:10 +0200

    game.c

--- PS3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
+++ PS3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
@@ -5,5 +5,14 @@ int main(int argc, char *argv[])
 {
 	printf("Hello World, I'm game or am I ?!\n");
 
+	FILE *file=fopen("/dev/gamepad-driver", "r");
+
+	while(1){
+		
+		
+
+	}
+
 	exit(EXIT_SUCCESS);
 }
+
